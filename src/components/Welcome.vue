90ca32d7894e84569fdc48108fcade3483bf7f48
jyj <jyjaiqzy@163.com>
2020-12-21 13:37:25 +0800

    完成优化

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -3,16 +3,8 @@
     <h3>Welcome to my vue_shop demo!</h3>
     <div>项目已经上传至我的码云仓库和github仓库</div>
     <div>
-      <p>
-        码云仓库地址：<a href="https://gitee.com/jyj-277/my-echarts_demo"
-          >https://gitee.com/jyj-277/my-echarts_demo</a
-        >
-      </p>
-      <p>
-        git地址：<a href="https://github.com/jyj-277/vue_shop"
-          >https://github.com/jyj-277/vue_shop</a
-        >
-      </p>
+      <p>码云仓库地址：<a href="https://gitee.com/jyj-277/vue_shop">https://gitee.com/jyj-277/vue_shop</a></p>
+      <p>git地址：<a href="https://github.com/jyj-277/vue_shop">https://github.com/jyj-277/vue_shop</a></p>
     </div>
   </div>
 </template>
